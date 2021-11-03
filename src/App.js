22fddbb220f7dee934712c54909b6956cfd18b8f
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2021-11-03 15:46:28 +0200

    action creators

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/ui-slice';
+import { sendCartData, fetchCartData } from './store/cart-actions';
 
 let isInitial = true;
 
@@ -16,44 +16,15 @@ function App() {
 	const notification = useSelector((state) => state.ui.notification);
 
 	useEffect(() => {
-		const sendCartData = async () => {
-			dispatch(
-				uiActions.showNotification({
-					status: 'pending',
-					title: 'Sending...',
-					message: 'Sending cart data!'
-				})
-			);
+		dispatch(fetchCartData());
+	}, [dispatch]);
 
-			const response = await fetch(
-				'https://react-http-ecb4c-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-				{ method: 'PUT', body: JSON.stringify(cart) }
-			);
-
-			if (!response.ok) {
-				throw new Error('Sending cart data failed!');
-			}
-
-			dispatch(
-				uiActions.showNotification({
-					status: 'success',
-					title: 'Success!',
-					message: 'Sent cart data successfully!'
-				})
-			);
-		};
-
+	useEffect(() => {
 		if (isInitial) return (isInitial = false);
 
-		sendCartData().catch((error) =>
-			dispatch(
-				uiActions.showNotification({
-					status: 'error',
-					title: 'Error!',
-					message: error.message
-				})
-			)
-		);
+		if (cart.changed) {
+			dispatch(sendCartData(cart));
+		}
 	}, [cart, dispatch]);
 
 	return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -8,13 +8,19 @@ const initialState = {
 const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
+	changed: false,
 	reducers: {
+		replaceCart(state, action) {
+			state.totalQuantity = action.payload.totalQuantity;
+			state.items = action.payload.items;
+		},
 		addItemToCart(state, action) {
 			const newItem = action.payload;
 			const existingItem = state.items.find(
 				(item) => item.id === newItem.id
 			);
 			state.totalQuantity++;
+			state.changed = true;
 
 			if (!existingItem) {
 				state.items.push({
@@ -34,6 +40,7 @@ const cartSlice = createSlice({
 			const id = action.payload;
 			const existingItem = state.items.find((item) => item.id === id);
 			state.totalQuantity--;
+			state.changed = true;
 
 			if (existingItem.quantity === 1) {
 				state.items = state.items.filter((item) => item.id !== id);
